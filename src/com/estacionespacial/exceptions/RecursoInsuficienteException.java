00e8569ad466e3a9575e33005bf8056ff1a35d97
Jesús Cantos <jclinspain2005@gmail.com>
2025-06-16 09:38:23 +0200

    Create RecursoInsuficienteException.java

---
+++ src/com/estacionespacial/exceptions/RecursoInsuficienteException.java
@@ -0,0 +1,8 @@
+</details> <details> <summary><code>RecursoInsuficienteException.java</code></summary>
+package com.estacionespacial.exceptions;
+
+public class RecursoInsuficienteException extends Exception {
+    public RecursoInsuficienteException(String mensaje) {
+        super(mensaje);
+    }
+}
