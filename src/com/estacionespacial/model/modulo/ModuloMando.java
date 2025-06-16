3287d7e00e44b370fa1ba53a5385014db303b192
Jesús Cantos <jclinspain2005@gmail.com>
2025-06-16 09:50:58 +0200

    Create ModuloMando.java

---
+++ src/com/estacionespacial/model/modulo/ModuloMando.java
@@ -0,0 +1,25 @@
+<details> <summary><code>ModuloMando.java</code></summary>
+package com.estacionespacial.model.modulo;
+
+public class ModuloMando extends Modulo {
+    private boolean tieneIA;
+
+    public ModuloMando(String codigo, String nombre, int cap, boolean op, boolean ia) {
+        super(codigo, nombre, cap, op);
+        this.tieneIA = ia;
+    }
+
+    public boolean isTieneIA() { return tieneIA; }
+    public void setTieneIA(boolean ia) { this.tieneIA = ia; }
+
+    @Override
+    public String getTipo() { return "Mando"; }
+
+    @Override
+    public String toCSV() {
+        return String.join(";", getCodigo(), getNombre(), getTipo(),
+                           String.valueOf(getCapacidad()), String.valueOf(isOperativo()),
+                           String.valueOf(tieneIA));
+    }
+}
+</details>
