c99cdb3c469198046e86989c4cf575aa2763d531
Jesús Cantos <jclinspain2005@gmail.com>
2025-06-16 09:39:05 +0200

    Create ModuloInoperativoException.java

---
+++ src/com/estacionespacial/exceptions/ModuloInoperativoException.java
@@ -0,0 +1,9 @@
+</details> <details> <summary><code>ModuloInoperativoException.java</code></summary>
+package com.estacionespacial.exceptions;
+
+public class ModuloInoperativoException extends Exception {
+    public ModuloInoperativoException(String mensaje) {
+        super(mensaje);
+    }
+}
+</details>
