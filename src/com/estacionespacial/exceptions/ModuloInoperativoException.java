78915926a13b11f325d5ee49815f82f03ed6d2b0
Jesús Cantos <jclinspain2005@gmail.com>
2025-06-16 09:49:27 +0200

    Update ModuloInoperativoException.java

--- src/com/estacionespacial/exceptions/ModuloInoperativoException.java
+++ src/com/estacionespacial/exceptions/ModuloInoperativoException.java
@@ -1,4 +1,4 @@
-</details> <details> <summary><code>ModuloInoperativoException.java</code></summary>
+<details> <summary><code>ModuloInoperativoException.java</code></summary>
 package com.estacionespacial.exceptions;
 
 public class ModuloInoperativoException extends Exception {
