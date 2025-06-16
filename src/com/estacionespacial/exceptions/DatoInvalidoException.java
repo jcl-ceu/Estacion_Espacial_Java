12b8761e58195797489234356f563b6698037896
Jesús Cantos <jclinspain2005@gmail.com>
2025-06-16 09:39:41 +0200

    Update and rename com.estacionespacial.exceptions to src/com/estacionespacial/exceptions/DatoInvalidoException.java

---
+++ src/com/estacionespacial/exceptions/DatoInvalidoException.java
@@ -0,0 +1,8 @@
+<details> <summary><code>DatoInvalidoException.java</code></summary>
+package com.estacionespacial.exceptions;
+
+public class DatoInvalidoException extends Exception {
+    public DatoInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
